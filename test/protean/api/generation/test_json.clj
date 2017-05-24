05b48ee583830029a57c19825ab214a8d1ee23e9
rossputin <rossajmcd@gmail.com>
2017-05-24 20:07:20 +0100

    convert json generation stdout to expectations tests

--- test/protean/api/generation/test_json.clj
+++ test/protean/api/generation/test_json.clj
@@ -1,10 +1,13 @@
 (ns protean.api.generation.test-json
   (:require [cheshire.core :as cc]
             [protean.api.generation.json :refer [gen]]
+            [protean.api.transformation.coerce :refer [clj]]
             [expectations :refer :all]))
 
-(let [json (gen "test-data/schemas/pet.schema.json")]
-  (println json))
+(let [json (gen "test-data/schemas/pet.schema.json")
+      c (clj json true)]
+  (expect (:status c) (in #{"available", "pending", "sold"})))
 
-(let [json (gen "test-data/schemas/test.schema.json")]
-  (println (cc/generate-string (cc/parse-string json) {:pretty true})))
+(let [json (gen "test-data/schemas/test.schema.json")
+      c (clj json true)]
+  (expect (:enum c) (in #{"one", "two", "three"})))
