bd0bef77f1376dde74610da08e2154f8c0eaacd4
Nick Featch <Nick.Featch@passivsystems.com>
2017-10-26 15:26:14 +0100

    #253 fix json coerce to account for body being just a boolean

--- src/protean/api/transformation/coerce.clj
+++ src/protean/api/transformation/coerce.clj
@@ -8,6 +8,11 @@
             [yaclot.core :refer [convert to-type]])
   (:use [clojure.pprint :only [pprint]]))
 
+; Note must preserve nil since nil comparisons are used throughout protean core
+(defn- rich-generate-string [d keys]
+  (let [s (jsn/generate-string d keys)]
+    (if (.contains ["null" "\"\""] s) nil s)))
+
 ;; =============================================================================
 ;; Transformation functions
 ;; =============================================================================
@@ -16,9 +21,9 @@
 
 (defn long [s] (convert s (to-type Long)))
 
-(defn js [d] (if d (jsn/generate-string d) d))
+(defn js [d] (rich-generate-string d {:pretty false}))
 
-(defn pretty-js [d] (if d (jsn/generate-string d {:pretty true}) d))
+(defn pretty-js [d] (rich-generate-string d {:pretty true}))
 
 (defn clj
   ([d] (if (map? d) d (jsn/parse-string d)))
