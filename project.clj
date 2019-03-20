32ff49b1f02607a58d4a47ae9e4dbd6f3445753d
Nick Featch <Nick.Featch@passivsystems.com>
2019-03-20 15:31:49 +0000

    version 0.14.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protean-api "0.13.0"
+(defproject protean-api "0.14.0-SNAPSHOT"
   :description "Protean API."
   :url "http://github.com/passivsystems/protean-api"
   :license {:name "Apache License v2.0"
